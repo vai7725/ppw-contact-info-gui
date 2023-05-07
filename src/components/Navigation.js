e6574505429f042e9cdfcc7196cc4b3c47e000a1
vai7725 <vaibhav.prajapat.7725@gmail.com>
2023-05-07 12:17:07 +0530

    Navbar title fixed

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,7 @@ import { Navbar, Nav } from 'react-bootstrap';
 const Navigation = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand href="#home">My Website</Navbar.Brand>
+      <Navbar.Brand href="#home">Previous Papers | Contact info</Navbar.Brand>
     </Navbar>
   );
 };
